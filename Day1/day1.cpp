2fdfbbb5dc7aaba054bf37a8f4f580f33d0593d1
Clement Lan <clement.lan@smiths-detection.com>
2018-12-07 21:42:13 -0800

    day1 done

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unordered_set>
 
 int main(int argc, char** argv) {
 
@@ -13,12 +14,30 @@ int main(int argc, char** argv) {
 
   int freq = 0;
 
-  char  in_buff[256];
-  while(input_file.getline(in_buff, 256)) {
-    if (in_buff[0] == '+') { freq += atoi(in_buff+1); }
-    else if (in_buff[0] == '-') { freq -= atoi(in_buff+1); }
-    std::cout << freq << std::endl;
-  }
+  std::unordered_set<int> freq_list;
+  //we start with 0
+  freq_list.insert(0);
 
-  input_file.close();
+  char  in_buff[256];
+
+  while(1) {
+    std::cerr << "IsEOF: " << input_file.eof() << std::endl;
+    if (input_file.getline(in_buff, 256)) {
+      if (in_buff[0] == '+') { freq += atoi(in_buff+1); }
+      else if (in_buff[0] == '-') { freq -= atoi(in_buff+1); }
+      std::cout << "Freq: " << freq << std::endl;
+
+      if (freq_list.count(freq) == 1) {
+        std::cout << "Found repeat freq: " << freq << std::endl;
+        return 0;
+      } else {
+        freq_list.insert(freq);
+      }
+    }
+    else {
+      std::cerr << "EOF, repeating" << std::endl;
+      input_file.clear();
+      input_file.seekg(0, input_file.beg);
+    }
+  }
 }
